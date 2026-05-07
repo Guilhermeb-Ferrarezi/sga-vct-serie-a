27b3c61ec4c183f5e7b44690869895d26b78f8eb
guilherme <guibferrarezi@gmail.com>
2026-05-07 16:13:52 -0300

    Compress OG image for VCT Serie A

--- src/routes/vct-ribeirao/serie-a.tsx
+++ src/routes/vct-ribeirao/serie-a.tsx
@@ -11,7 +11,7 @@ import { FinalCta } from "@/components/site/FinalCta";
 import { Footer } from "@/components/site/Footer";
 import { SignupModal } from "@/components/site/SignupModal";
 
-const ogImage = `${import.meta.env.BASE_URL}vct-serie-a.png`;
+const ogImage = `${import.meta.env.BASE_URL}vct-serie-a.jpg`;
 
 export const Route = createFileRoute("/vct-ribeirao/serie-a")({
   headers: () => ({
